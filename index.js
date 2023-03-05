31370ae0f8e4e5e41745682b73ce74e5c296eb99
Red <red.sarouf2014@gmail.com>
2023-03-05 05:43:42 -0500

    added functions lab code

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    const phrase = string.toUpperCase();
+    console.log(phrase)
+}
+
+function logWhisper(string) {
+    const phrase = string.toLowerCase();
+    console.log(phrase)
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if(string === string.toLowerCase()){
+        return  "I can't hear you!";
+    }else if(string === string.toUpperCase()){
+        return  "YES INDEED!";
+    } else if(string === "Let's have dinner together!"){
+        return "I would love to!";
+    }
+    
+}
